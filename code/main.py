f2468e18bdf359abfad7b93601eeb6e22bd86753
Aramoscelaya <aramos.software@gmail.com>
2023-05-31 11:47:40 -0500

    Formato a Menu, renombrado de archivo Menu->main y armado de estructura para espacios defunciones

--- code/main.py
+++ code/main.py
@@ -1,13 +1,14 @@
-#Archivo prueba para verificar que el repositorio funciona de manera correcta
-
 def mostrar_menu(opciones): #Función para recorrer el diccionario
-    print('Seleccione la opción deseada:')
+    print("\n\t\t\t\t\t\tMENÚ\n")
+    print('\t\t',("-"*60))
+    print(f"\t\t{'| Seleccione la opción deseada:':61}|")
     for i in sorted(opciones): #sorted ordena el diccionario para que las opciones se muestren de manera ordenada
-        print(f' {i}) {opciones[i][0]}')
+        print(f'\t\t| \t{i}) {opciones[i][0]:50}|')
+    print('\t\t',("-"*60))
 
 def leer_opcion(opciones): #Función que lee unicamente la opción ingresada por el usuario
     while (a := input(' Opción: ')) not in opciones:
-        print('Opción inválida')
+        print('>> Opción inválida\n')
     return a
 
 def ejecutar(opcion, opciones): #recibirá la opción seleccionada y el diccionario de opción  ejecutará la acción correspondiente
@@ -22,7 +23,6 @@ def generar_menu(opciones,salida):
         print() # se imprime una línea en blanco para clarificar la salida por pantalla
 
 def menu():
-   print("\n\t\tMENÚ\n")
    opciones={
    '1':('Día con más casos a nivel nacional',funcion1),
    '2':('% Casos confirmados de acuerdo a la población',funcion2),
@@ -33,12 +33,16 @@ def menu():
 
 def salir():
     print("Ha salido del programa")
+
+########## Importar los archivos que tengan el desarrollo segun la opcion ##########
 def funcion1():
-    pass
+    pass 
+
 def funcion2():
-    pass
+    pass 
+
 def funcion3():
-    pass
+    pass 
 
 def main():
     menu()
